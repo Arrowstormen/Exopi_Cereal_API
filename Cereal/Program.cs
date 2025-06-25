9f0c6633305d8fa7e8804cf2dc6dccbbc9ae88d4
Mikkel <arrowstorm.videos@gmail.com>
2025-06-25 16:25:58 +0200

    More database setup

--- Cereal/Program.cs
+++ Cereal/Program.cs
@@ -1,3 +1,5 @@
+using Cereal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,14 +9,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<CerealContext>();
+using (var context = new CerealContext())
+{
+    context.Database.EnsureCreated();
+}
+
+//Register services
+var services = builder.Services;
+
+services.AddTransient<ICerealContext, CerealContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 

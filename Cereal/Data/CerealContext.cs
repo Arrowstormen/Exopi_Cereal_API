2be93ba2687413dc9dd20585393cf107a9299833
Mikkel <arrowstorm.videos@gmail.com>
2025-06-30 15:54:51 +0200

    Working on parsing data in CerealContext

--- Cereal/Data/CerealContext.cs
+++ Cereal/Data/CerealContext.cs
@@ -1,4 +1,9 @@
-﻿using Cereal.Models;
+﻿using System.Globalization;
+using System.Reflection;
+using System.Text;
+using Cereal.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cereal.Data
@@ -8,9 +13,43 @@ namespace Cereal.Data
         public DbSet<CerealEntity> Cereals { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=cerealdb.db");
-        }
+            => optionsBuilder
+                .UseSqlite("Data Source=cerealdb.db")
+                .UseSeeding((context, _) =>
+                {
+                    string resourceName = Path.Combine("SeedData", "cereal.csv");
+                    using (StreamReader reader = new StreamReader(resourceName, Encoding.UTF8))
+                    {
+                        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                        {
+                            MissingFieldFound = null,
+                            PrepareHeaderForMatch = args => args.Header.ToLower(),
+                            ShouldSkipRecord = args =>
+                            {
+                                var rawRow = args.Row.Parser.RawRow;
+                                return rawRow == 2; //Ignore second row of types
+                            }
+                            
+                        }; //Do not perceive Id? mfr not understood?
+                        CsvReader csvReader = new CsvReader(reader, config);
+                        var cereals = csvReader.GetRecords<CerealEntity>().ToArray();
+                        foreach (var cereal in cereals) 
+                        {
+                            var existingCereal = Cereals.FirstOrDefault(c => c.Name == cereal.Name);
+                            if (existingCereal == null)
+                            {
+                                Cereals.Add(cereal);
+                            } 
+                            else
+                            {
+                                cereal.Id = existingCereal.Id;
+                                Cereals.Update(cereal);
+                            }
+                        }
+                        SaveChangesAsync();
+                    }
+                });
+        
 
     }
 }
--- Cereal/Models/Cereal.cs
+++ Cereal/Models/Cereal.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Cereal.Models.Types;
+using CsvHelper.Configuration.Attributes;
 
 namespace Cereal.Models
 {
@@ -8,47 +9,64 @@ namespace Cereal.Models
         public int Id { get; set; }
 
         [Required]
+        [Index(0)]
         public string Name { get; set; }
 
+        [Name("mfr")]
+        [Index(1)]
         public Manufacturer Manufacturer { get; set; }
 
+        [Index(2)]
         public TempType Type { get; set; }
 
+        [Index(3)]
         //Per serving
         public int Calories { get; set; }
 
+        [Index(4)]
         //Grams
         public int Protein { get; set; }
 
+        [Index(5)]
         //Grams
         public int Fat {  get; set; }
 
+        [Index(6)]
         //Milligrams
         public int Sodium { get; set; }
 
+        [Index(7)]
         //Grams
         public float Fiber { get; set; }
 
+        [Index(8)]
         //Grams
         public float Carbo { get; set; }
 
+        [Index(9)]
         //Grams
         public int Sugars { get; set; }
 
+        [Index(10)]
         //Milligrams
         public int Potass { get; set; }
 
+        [Index(11)]
         //Percentage of 
         public int Vitamins { get; set; }
 
+        [Index(12)]
         //Which display shelf
         public int Shelf { get; set; }
 
+        [Index(13)]
         //Ounces
         public float Weight { get; set; }
 
+        [Index(14)]
         public float Cups { get; set; }
 
+        [Index(15)]
         public float Rating { get; set; }
     }
 }

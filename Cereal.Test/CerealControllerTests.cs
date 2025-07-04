ad4af70efdfe9b7862206850d37bb16845d85f82
Mikkel <arrowstorm.videos@gmail.com>
2025-07-04 15:53:02 +0200

    Getting controller tests class ready.

---
+++ Cereal.Test/CerealControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Cereal.Controllers;
+using Cereal.Services;
+using Moq;
+
+namespace Cereal.Test
+{
+    [TestClass]
+    public class CerealControllerTests
+    {
+        private static CerealController GetController(Mock<ICerealService> cerealService) =>
+        new(cerealService.Object);
+
+        [TestMethod]
+        public async Task GetAllCereals()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
